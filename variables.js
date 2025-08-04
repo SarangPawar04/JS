530ee3867a3ba49dac805d4f2c448a6c267bd586
SarangPawar04 <126444017+SarangPawar04@users.noreply.github.com>
2025-08-04 10:05:00 +0530

    Js lectures

---
+++ variables.js
@@ -0,0 +1,10 @@
+const accountId = 12345
+let accountMail = "saranggamil.com"
+var accountPassowrd = "sarang11"
+
+account = "nagar"
+
+console.log(accountId);
+
+
+console.table([accountId, accountMail, accountPassowrd, account])
